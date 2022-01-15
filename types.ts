6ef5aba86de3c332aaf6a49db693d43fbba1c08f
encorey <kosz.kosz@protonmail.com>
2022-01-15 20:39:38 +0100

    added insertToTable function

---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface TObject {
+  currency: string,
+  rate: string,
+  bid: string,
+  ask: string,
+  high: string,
+  low: string,
+  open: string,
+  close: string,
+  timestamp: string,
+}
