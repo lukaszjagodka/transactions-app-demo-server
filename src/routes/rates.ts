17a338d1f959341d305c8a874b97ff09465222f0
encorey <kosz.kosz@protonmail.com>
2022-01-18 20:36:20 +0100

    refactored cron service

--- src/routes/rates.ts
+++ src/routes/rates.ts
@@ -1,24 +1,15 @@
 import { Request, Response, Router } from 'express';
-import * as pg from "pg";
+import client from '../services/dbConnection';
 
 const router = Router()
 
-const conString = process.env.CON_STRING;
-const client = new pg.Client(conString);
-
 router.get('/', (req: Request, res: Response) => {
-  client.connect(function(err: any) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
-    client.query(`SELECT * FROM "public"."currencies" LIMIT 1000`, function(err: Error, result: { rows: any }) {
+    client.query(`SELECT * FROM "currencies" ORDER BY id DESC LIMIT 1`, function(err: Error, result: { rows: any }) {
       if(err) {
         return console.error('error running query', err);
       }
-      client.end();
       return res.json(result.rows);
     });
-  });
 })
 
 export default router
--- src/services/cron.ts
+++ src/services/cron.ts
@@ -1,8 +1,30 @@
 import { CronJob } from 'cron';
-import insertToTable from '../helpers/insertToTable';
+import { IRow } from '../../types';
+import { fetch3x } from '../helpers/fetchCurrencies';
+import client from './dbConnection';
 
-const job = new CronJob('*/20 * * * *', function() {
-  insertToTable()
+const job = new CronJob('*/20 * * * *', async function() {
+  try{
+    const data: Array<IRow> = await fetch3x();
+    if(data.length > 1){
+        let query = '';
+        for(let i=0; i<data.length; i++){
+          query+=`"("${data[i].currency}",${data[i].rate})",`
+        }
+        query = query.slice(0, -2);
+        query = query.substring(1);
+        const sqlQuery = `INSERT INTO currencies (currencyString, createdAt) VALUES ('[${query}]', CURRENT_TIMESTAMP)`
+          client.query(sqlQuery, function(err: any, result: any) {
+            if(err) {
+              return console.error('error running query', err);
+            }
+          });
+    }else{
+      console.log('Limit of 3 requests/hour have been reached')
+    }
+  }catch(err: any){
+    console.log(err)
+  }
 }, null, true, 'Europe/Warsaw');
 
 export default job;
