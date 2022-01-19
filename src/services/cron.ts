80c01be386e3ebfd0297c92d5e67fb67b0c52ebd
encorey <kosz.kosz@protonmail.com>
2022-01-19 11:17:31 +0100

    refactored insert to db on typeorm

--- src/services/cron.ts
+++ src/services/cron.ts
@@ -1,28 +1,28 @@
-import { CronJob } from 'cron';
-import { IRow } from '../../types';
+import { connection } from '../services/ormConnection';
 import { fetch3x } from '../helpers/fetchCurrencies';
-import client from './dbConnection';
+import { Currency } from "../entity/Currency";
+import { IRow } from '../../types';
+import { CronJob } from 'cron';
 
 const job = new CronJob('*/20 * * * *', async function() {
   try{
     const data: Array<IRow> = await fetch3x();
     if(data.length > 1){
-        let query = '';
-        for(let i=0; i<data.length; i++){
-          query+=`"("${data[i].currency}",${data[i].rate})",`
-        }
-        query = query.slice(0, -2);
-        query = query.substring(1);
-        const sqlQuery = `INSERT INTO currencies (currencyString, createdAt) VALUES ('[${query}]', CURRENT_TIMESTAMP)`
-          client.query(sqlQuery, function(err: any, result: any) {
-            if(err) {
-              return console.error('error running query', err);
-            }
-          });
+      let query = '';
+      for(let i=0; i<data.length; i++){
+        query+=`{"${data[i].currency}",${data[i].rate}},`
+      }
+      query = query.slice(0, -1)
+      connection.then(async connection => {
+        let currency = new Currency();
+        currency.currencyString = query;
+        currency.createdAt = new Date;
+        await connection.manager.save(currency);
+      }).catch(error => console.log(error));
     }else{
       console.log('Limit of 3 requests/hour have been reached')
     }
-  }catch(err: any){
+  }catch(err){
     console.log(err)
   }
 }, null, true, 'Europe/Warsaw');
