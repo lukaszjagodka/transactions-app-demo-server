38c6947db581ac664acfa2fb786fd66fbe579ccb
encorey <kosz.kosz@protonmail.com>
2022-01-29 18:00:30 +0100

    added bodyParser

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import helmet from 'helmet';
 import 'reflect-metadata';
 import 'dotenv/config';
-
+import bodyParser from 'body-parser';
 import rates from './src/controllers/rates';
 import job from './src/services/cron';
 
 const app = express();
 const port = 3001;
+
+app.use(bodyParser.json());
 
 app.use(helmet());
 app.use('/rates', rates);
