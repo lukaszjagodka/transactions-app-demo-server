5cea5941174dd0f8dce210e568fa9a99629e8815
encorey <kosz.kosz@protonmail.com>
2022-01-29 23:55:17 +0100

    type for new account

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,3 +14,11 @@ export type TPair = {
   pair: string,
   value: string
 }
+
+export type IAccount = {
+  id: number
+  name: string,
+  accountNumber: number,
+  accountValue: number,
+  currency: string
+}
